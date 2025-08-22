d228ed63a66953b34ed1859c9f569f2534e17a13
rushikeshmusale86@gmail.com <rushikeshmusale86@gmail.com>
2025-08-22 18:22:33 +0530

    update server

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 let isConnected = false;
 
 async function connectDB() {
-  const MONGO_URI = 'mongodb://localhost:27017/mess-management';
+  const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mess-management';
   if (isConnected) return;
   await mongoose.connect(MONGO_URI); // Removed deprecated options
   isConnected = true;
